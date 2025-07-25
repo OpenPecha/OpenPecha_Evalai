71f59b94f01f576ffa37fa53fcdca60623534e32
Tenzin Kunsang <42644738+tenkus47@users.noreply.github.com>
2025-07-25 13:11:53 +0530

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -16,11 +16,13 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from database import Base
+from database import DATABASE_URL, Base
 from models import *
 
 target_metadata = Base.metadata
 
+config = context.config
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
